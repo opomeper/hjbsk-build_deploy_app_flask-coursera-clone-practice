3fda581f568df62092ed252ac86c054c7a394c66
opomeper <saurojoemer14@gmail.com>
2025-08-22 04:34:56 +0800

    Lab completed. Add Maths folder, mathemathics.py, __init__.py, and updated/completed the code in server.py

---
+++ Maths/mathematics.py
@@ -0,0 +1,11 @@
+def summation(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def subtraction(a, b):
+    """Returns the difference of two numbers."""
+    return a - b
+
+def multiplication(a, b):
+    """Returns the product of two numbers."""
+    return a * b
